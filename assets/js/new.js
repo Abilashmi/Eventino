fc07124e46a51fbee94f480d2f3f9e934568fe26
Abilashmi <abilashmi5161@gmail.com>
2024-11-05 10:25:32 +0530

    Add files via upload

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,5 +1,13 @@
-function navigateToSection(event, sectionId) {
-  event.preventDefault();
-  const section = document.getElementById(sectionId);
-  section.scrollIntoView({ behavior: 'smooth', inline: 'start' });
+function showSection(sectionId){
+  const hideSection = document.querySelectorAll('.section1');
+  hideSection.forEach(section => section.style.display = "none");
+
+  const activeSection = document.getElementById(sectionId)
+  if(activeSection){
+    activeSection.style.display= "block";
+  }
 }
+
+window.onload = function() {
+  showSection('home'); // Replace 'home' with the actual ID of your home section
+};
